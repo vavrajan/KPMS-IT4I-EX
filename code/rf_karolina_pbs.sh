f69fd5d4ab79e2f2092cd8a703dc1da8204d4ff5
vavrajan <vavrajan@github.com>
2022-03-02 18:40:41 +0100

    added cores

--- code/rf_karolina_pbs.sh
+++ code/rf_karolina_pbs.sh
@@ -15,3 +15,8 @@ time Rscript rf_serial.r
 time Rscript rf_mc.r 1
 time Rscript rf_mc.r 2
 time Rscript rf_mc.r 4
+time Rscript rf_mc.r 8
+time Rscript rf_mc.r 16
+time Rscript rf_mc.r 32
+time Rscript rf_mc.r 64
+time Rscript rf_mc.r 128
