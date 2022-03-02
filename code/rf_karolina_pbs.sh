4144ac083f98ca8e7fa9fe6f2692e5d745cb3a4d
George Ostrouchov <ostrouchovg@ornl.gov>
2022-03-02 01:21:57 -0500

    script name

--- code/rf_karolina_pbs.sh
+++ code/rf_karolina_pbs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N rf
-#PBS -l select=1:ncpus=128,walltime=00:01:00
+#PBS -l select=1:ncpus=4,walltime=00:05:00
 #PBS -q qexp
 #PBS -e rf.e
 #PBS -o rf.o
@@ -16,6 +16,7 @@ echo "loaded R"
 #export OMPI_MCA_mpi_warn_on_fork=0
 #export RDMAV_FORK_SAFE=1
 
-time Rscript 1
-time Rscript 2
-time Rscript 4
+time Rscript rf_serial.r
+time Rscript rf_mc.r 1
+time Rscript rf_mc.r 2
+time Rscript rf_mc.r 4
